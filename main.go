1bb09e87d1ccc20325ca1485a94a9844ff0ff561
sergey <sergey@intervolga.ru>
2026-08-21 12:22:44 +0300

    Убрал комменатрии

--- main.go
+++ main.go
@@ -17,28 +17,30 @@ func main() {
 
 	utxoDB := utxo.UTXODB{
 		utxo.UTXOKey{TxID: [32]byte{}, OutIndex: 0}: utxo.UTXOEntry{Output: coin.TxOutput{Amount: 5, PublicKey: myWallet.PublicKey}},
-		// utxo.UTXOKey{TxID: [32]byte{}, OutIndex: 1}: utxo.UTXOEntry{Output: coin.TxOutput{Amount: 3, PublicKey: myWallet.PublicKey}},
-		// utxo.UTXOKey{TxID: [32]byte{}, OutIndex: 2}: utxo.UTXOEntry{Output: coin.TxOutput{Amount: 11, PublicKey: myWallet.PublicKey}},
-		// utxo.UTXOKey{TxID: [32]byte{}, OutIndex: 3}: utxo.UTXOEntry{Output: coin.TxOutput{Amount: 10, PublicKey: myWallet.PublicKey}},
+		utxo.UTXOKey{TxID: [32]byte{}, OutIndex: 1}: utxo.UTXOEntry{Output: coin.TxOutput{Amount: 3, PublicKey: myWallet.PublicKey}},
+		utxo.UTXOKey{TxID: [32]byte{}, OutIndex: 2}: utxo.UTXOEntry{Output: coin.TxOutput{Amount: 11, PublicKey: myWallet.PublicKey}},
+		utxo.UTXOKey{TxID: [32]byte{}, OutIndex: 3}: utxo.UTXOEntry{Output: coin.TxOutput{Amount: 10, PublicKey: myWallet.PublicKey}},
+	}
+	mp := mempool.NewMempool()
+
+	t1, err := myWallet.CreateTransaction(otherWallet.PublicKey, 10, utxoDB)
+
+	if err != nil {
+		panic(err)
 	}
 
-	// t1, err := myWallet.CreateTransaction(otherWallet.PublicKey, 10, utxoDB)
-	// if err != nil {
-	// panic(err)
-	// }
-
 	t2, err := myWallet.CreateTransaction(otherWallet.PublicKey, 1, utxoDB)
+
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("было\n%v\n\n", utxoDB)
 
-	mp := mempool.NewMempool()
+	if err := mp.Add(t1, utxoDB); err != nil {
+		panic(err)
+	}
 
-	// if err := mp.Add(t1, utxoDB); err != nil {
-	// panic(err)
-	// }
 	if err := mp.Add(t2, utxoDB); err != nil {
 		panic(err)
 	}
